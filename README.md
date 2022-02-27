24594e7627e5d05f18cfe1db5c997ee135d39b93
vxfontes <nessa1vane@icloud.com>
2022-02-26 23:33:49 -0300

    colocando teste de componentes

--- README.md
+++ README.md
@@ -7,3 +7,9 @@
 - podemos usar mais de um describe para separar nossos testes em etapas ou subcategorias
 - precisa renderizar o componente onde se encontra o objeto de teste antes
 - para realizarmos teste de funções, precisamos implementar as variáveis necessárias antes e colocar a função
+
+#testando componentes react
+- cria um arquivo .test.js na própria pasta do componente
+- usar snapshot para conferencia de elementos dentro dos componentes
+    - cria uma pasta de snapshots com codigos html que experamos ter na maquina
+- usamos fireevent para simular eventos no banco de dados, testando assim o return de componentes
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 // renderizar elementos dos testes e screen para pega-los
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App, { calcularNovoSaldo } from './App'
 
 describe('componente principal', () => {
+
+    // teste elementos básicos
     describe('quando abro o banco, ', () => {
         it('mostra nome do banco', () => {
             // esperando que o titulo esteja lá
@@ -23,6 +25,7 @@ describe('componente principal', () => {
         })
     });
 
+    // teste funções
     describe('quando realizo transação:', () => {
         it('saque, o valor diminui', () => {
             const valores = {
@@ -35,5 +38,30 @@ describe('componente principal', () => {
             expect(novoSaldo).toBe(100);
         })
     });
-        
+    
+    it('que é um saque, a transação deve ser realizada', () => {
+        // iniciar o app para uso
+        render(<App />);
+
+        // pegar elemento pelo texto
+        // screen entrega tudo que o render pode oferecer
+        const saldo = screen.getByText('R$ 1000');
+        // pegar elemento pela label
+        const transacao = screen.getByLabelText('Saque');
+        // pegar elemento pelo testid: uma propriedade q colocamos dentro das tags para referencia-la no teste  
+        const valor = screen.getByTestId('valor');
+        const botaoTransacao = screen.getByText('Realizar operação');
+
+        // pega o valor contido 
+        expect(saldo.textContent).toBe('R$ 1000')
+
+        // fireevent simula um evento do banco
+        // aqui teremos um clique, onde selecionariamos a opção
+        fireEvent.click(transacao, { target: { value: 'saque' } }); 
+        // mudando o valor
+        fireEvent.change(valor, { target: { value: 10 } });
+        fireEvent.click(botaoTransacao);
+
+        expect(saldo.textContent).toBe('R$ 990');
+    })
 })
