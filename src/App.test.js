1baaa80cba07b317a0bac50c4b80e34c59c4fd48
vxfontes <nessa1vane@icloud.com>
2022-02-26 22:32:56 -0300

    teste de função

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // renderizar elementos dos testes e screen para pega-los
 import { render, screen } from '@testing-library/react';
-import App from './App'
+import App, { calcularNovoSaldo } from './App'
 
 describe('componente principal', () => {
     describe('quando abro o banco, ', () => {
@@ -21,5 +21,19 @@ describe('componente principal', () => {
             render(<App />)
             expect(screen.getByText('Realizar operação')).toBeInTheDocument();
         })
-    })
+    });
+
+    describe('quando realizo transação:', () => {
+        it('saque, o valor diminui', () => {
+            const valores = {
+                transacao: 'saque',
+                valor: 50,
+            }
+
+            // recebe a transação e o valor da conta
+            const novoSaldo = calcularNovoSaldo(valores, 150);
+            expect(novoSaldo).toBe(100);
+        })
+    });
+        
 })
